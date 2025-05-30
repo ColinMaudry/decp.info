e4a670948de07437cd6987d1aedce3599055e30b
Colin Maudry <colin@maudry.com>
2025-05-30 21:38:14 +0200

    Ajout du code de suivi Matomo

--- src/app.py
+++ src/app.py
@@ -4,6 +4,36 @@ import dash_bootstrap_components as dbc
 
 app = Dash(external_stylesheets=[dbc.themes.UNITED], title="decp.info", use_pages=True)
 
+app.index_string = """
+<!DOCTYPE html>
+<html>
+    <head>
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    <script type="application/javascript">var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+    var u="//analytics.maudry.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '14']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
+    </body>
+</html>
+"""
 
 app.layout = html.Div(
     [
