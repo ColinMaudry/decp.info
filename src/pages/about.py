313a7ba23050a608665231ee4a40e36134ead3b0
Colin Maudry <colin@maudry.com>
2025-06-02 20:25:45 +0200

    A propos du projet

--- src/pages/about.py
+++ src/pages/about.py
@@ -1,4 +1,4 @@
-from dash import register_page, html
+from dash import register_page, html, dcc
 
 title = "À propos"
 
@@ -6,4 +6,24 @@ register_page(
     __name__, path="/a-propos", title=f"decp.info - {title}", name=title, order=3
 )
 
-layout = [html.H2(title)]
+layout = [
+    html.Div(
+        className="container",
+        children=[
+            html.H2(title),
+            dcc.Markdown(
+                """Outil développé par Colin Maudry, sous licence GPL v3 (libre et gratuit).
+
+- [wiki du projet](https://github.com/ColinMaudry/decp-processing/wiki)
+- [code source de decp.info](https://github.com/ColinMaudry/decp.info)
+- [code source du traitement de données](https://github.com/ColinMaudry/decp-processing)
+
+Contact :
+- [colin+decp@maudry.com](mailto:colin+decp@maudry.com)
+- BlueSky : [@col1m.bsky.social](https://bsky.app/profile/col1m.bsky.social)
+- Mastodon : [col1m@mamot.fr](https://mamot.fr/@col1m)
+"""
+            ),
+        ],
+    )
+]
