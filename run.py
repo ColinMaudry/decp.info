62eb4d98f051a1d8a5b6604ecc023a22549aa472
Colin Maudry <colin@maudry.com>
2026-05-05 15:32:27 +0200

    Correction de l'ajout de CORS

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from flask_cors import CORS
 from src.app import app
 
 # To use `gunicorn run:server` (prod)
-server = CORS(app.server)
+server = app.server
+CORS(server)
 
 # To use `python run.py` (dev)
 if __name__ == "__main__":
