41491159a34c79ef44cac25a1851151809d1f39a
Colin Maudry <colin@maudry.com>
2026-06-30 22:41:19 +0200

    Ajouts des icones colibre #57

--- src/app.py
+++ src/app.py
@@ -166,7 +166,11 @@ app.index_string = """
     <head>
         {%metas%}
         <title>{%title%}</title>
-        {%favicon%}
+        <link rel="shortcut icon" href="/assets/icons/favicon.ico">
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
+        <link rel="manifest" href="/assets/icons/site.webmanifest">
         {%css%}
         <!-- canonical link -->
     </head>
