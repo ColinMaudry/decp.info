f3bbf38d80e114c4dc18c2d82631316499ccc079
Colin Maudry <colin@maudry.com>
2026-07-31 16:39:30 +0200

    VOTES_PER_WEEK en env (int)

--- src/subscriptions/db.py
+++ src/subscriptions/db.py
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS subscriber_state (
 _ACCESS_STATUSES = ("trial", "active")
 
 INITIAL_VOTES = 3
-VOTES_PER_WEEK = os.getenv("VOTES_PER_WEEK", 5)
+VOTES_PER_WEEK = int(os.getenv("VOTES_PER_WEEK", "5"))
 WEEK_SECONDS = 7 * 24 * 3600
 
 
